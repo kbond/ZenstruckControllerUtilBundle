897d5768c85a34f8da27d9f90fb5a847c2e2398f
Kevin Bond <kevinbond@gmail.com>
2014-10-15 11:03:37 -0400

    added checks for enabling param converters

--- DependencyInjection/ZenstruckControllerUtilExtension.php
+++ DependencyInjection/ZenstruckControllerUtilExtension.php
@@ -16,7 +16,16 @@ class ZenstruckControllerUtilExtension extends Extension
     {
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('listeners.xml');
-        $loader->load('param_converters.xml');
+        $loader->load('param_converters/session.xml');
+        $loader->load('param_converters/flash_bag.xml');
+
+        if ($container->hasParameter('security.context.class')) {
+            $loader->load('param_converters/security_context.xml');
+        }
+
+        if ($container->hasParameter('form.factory.class')) {
+            $loader->load('param_converters/form_factory.xml');
+        }
 
         $bundles = $container->getParameter('kernel.bundles');
 
